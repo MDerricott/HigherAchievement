69dcd3cfd311808f2638522b7999469cbf4a6248
MDerricott <mderricott@higherachievement.org>
2019-01-12 13:37:31 -0500

    Heatmap

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,45 @@
 import React, { Component } from "react";
 // import Virginia from "./pages/Virginia/index";
-import National from "./pages/National";
-import Center from "./pages/Center";
-import LandingPage from "./pages/LandingPage";
-import Richmond from "./pages/Richmond";
-import DCMetro from "./pages/DCMetro";
-import Pittsburgh from "./pages/Pittsburgh";
-import Baltimore from "./pages/Baltimore";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Navbar from "./common/Navbar";
-import NoMatch from "./pages/NoMatch";
+// import National from "./pages/National";
+// import Center from "./pages/Center";
+// import LandingPage from "./pages/LandingPage";
+// import Richmond from "./pages/Richmond";
+// import DCMetro from "./pages/DCMetro";
+// import Pittsburgh from "./pages/Pittsburgh";
+// import Baltimore from "./pages/Baltimore";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import Navbar from "./common/Navbar";
+// import NoMatch from "./pages/NoMatch";
+
 // import BottomNav from "./common/BottomNav";
+
+import Heatmap from "../src/pages/Center/Heatmap"
 
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div>
-          <Navbar />
-          <Switch>
-            <Route exact path="/" component={LandingPage} />
-            <Route exact path="/richmond" component={Richmond} />
-            <Route exact path="/dcmetro" component={DCMetro} />
-            <Route exact path="/pittsburgh" component={Pittsburgh} />
-            <Route exact path="/baltimore" component={Baltimore} />
-            <Route exact path="/center/:centerName" component={Center} />
-            <Route exact path="/national" component={National} />
-            <Route component={NoMatch} />
-          </Switch>
+
+          <Heatmap />
+      // <Router>
+      //   <div>
+      //     <Navbar />
+      //     <Switch>
+      //       <Route exact path="/" component={LandingPage} />
+      //       <Route exact path="/richmond" component={Richmond} />
+      //       <Route exact path="/dcmetro" component={DCMetro} />
+      //       <Route exact path="/pittsburgh" component={Pittsburgh} />
+      //       <Route exact path="/baltimore" component={Baltimore} />
+      //       <Route exact path="/center/:centerName" component={Center} />
+      //       <Route exact path="/national" component={National} />
+      //       <Route component={NoMatch} />
+      //     </Switch>
 
 
-          {/* <BottomNav /> */}
-        </div>
+   
+      //   </div>
         
-      </Router>
+      // </Router>
      
     );
   }
